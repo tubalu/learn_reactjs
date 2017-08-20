1146f14576dea6162bc2703cb206707b89778d78
scott lu <qingcai@gmail.com>
2017-08-19 21:05:56 -0700

    fix circle

--- src/components/circle/circlePage.js
+++ src/components/circle/circlePage.js
@@ -8,14 +8,12 @@ var Label = React.createClass({
     var labelStyle = {
 			fontFamily: "sans-serif",
 			fontWeight: "bold",
-            display: "inline-block",
-			padding: 10,
+			paddingTop: 5,
+			paddingLeft: 22,
 			margin: 0
 		};
     return (
-        <div> 
-            <p style={labelStyle}> {this.props.bgColor} </p>
-        </div>
+        <p style={labelStyle}> {this.props.bgColor} </p>
     );
   }
 });
@@ -23,9 +21,6 @@ var Label = React.createClass({
 var Circle = React.createClass({
     render: function () {
         var circleStyle = {
-            padding: 10,
-            margin: 20,
-            display: "inline-block",
             backgroundColor: this.props.bgColor,
             borderRadius: "50%",
             width: 100,
@@ -34,18 +29,33 @@ var Circle = React.createClass({
             filter: "drop-shadow(0px 0px 5px #666)"
         };
         return (
-            <div style={circleStyle}>
-            </div>
+            <div style={circleStyle}> </div>
         );
     }
 });
 
+var Card = React.createClass({
+    render: function(){
+        var cardStyle = {
+            padding: 10,
+            margin: 10,
+            display: "inline-block"
+        };
+      return (
+        <div style={cardStyle} >
+            <Circle bgColor={this.props.color} />
+            <Label bgColor={this.props.color} />
+        </div>
+      );
+    }
+  });
+
 var Outer = React.createClass({
     render: function () {
-        var colors = ["#58B3FF", "#FF605F", "#FFD52E", "#49DD8E", "#AE99FF", "#4cc3d9", "#cf6cc7", "#db6597", "#4babbd", "#ea9be1", "#ffb6c1", "#ffe4e1", "#f2f2f3", "#ebd3c7", "#5ba68c", "#ebef89", "#92eacc", "#62d7d2", "#abb1e8", "#d0e4b7", "#58B3FF", "#FF605F", "#FFD52E", "#49DD8E", "#AE99FF", "#4cc3d9", "#cf6cc7", "#db6597", "#4babbd", "#ea9be1", "#ffb6c1", "#ffe4e1", "#f2f2f3", "#ebd3c7", "#5ba68c", "#ebef89", "#92eacc", "#62d7d2", "#abb1e8", "#d0e4b7", "#58B3FF", "#FF605F", "#FFD52E", "#49DD8E", "#AE99FF", "#4cc3d9", "#cf6cc7", "#db6597", "#4babbd", "#ea9be1", "#ffb6c1", "#ffe4e1", "#f2f2f3", "#ebd3c7", "#5ba68c", "#ebef89", "#92eacc", "#62d7d2", "#abb1e8", "#d0e4b7", "#58B3FF", "#FF605F", "#FFD52E", "#49DD8E", "#AE99FF", "#4cc3d9", "#cf6cc7", "#db6597", "#4babbd", "#ea9be1", "#ffb6c1", "#ffe4e1", "#f2f2f3", "#ebd3c7", "#5ba68c", "#ebef89", "#92eacc", "#62d7d2", "#abb1e8", "#d0e4b7"];
+        var colors = ["#58B3FF", "#FF605F", "#FFD52E", "#49DD8E", "#AE99FF", "#4CC3D9", "#CF6CC7", "#DB6597", "#4BABBD", "#EA9BE1", "#FFB6C1", "#FFE4E1", "#F2F2F3", "#EBD3C7", "#5BA68C", "#EBEF89", "#92EACC", "#62D7D2", "#ABB1E8", "#D0E4B7", "#58B3FF", "#FF605F", "#FFD52E", "#49DD8E", "#AE99FF", "#4CC3D9", "#CF6CC7", "#DB6597", "#4BABBD", "#EA9BE1", "#FFB6C1", "#FFE4E1", "#F2F2F3", "#EBD3C7", "#5BA68C", "#EBEF89", "#92EACC", "#62D7D2", "#ABB1E8", "#D0E4B7", "#58B3FF", "#FF605F", "#FFD52E", "#49DD8E", "#AE99FF", "#4CC3D9", "#CF6CC7", "#DB6597", "#4BABBD", "#EA9BE1", "#FFB6C1", "#FFE4E1", "#F2F2F3", "#EBD3C7", "#5BA68C", "#EBEF89", "#92EACC", "#62D7D2", "#ABB1E8", "#D0E4B7", "#58B3FF", "#FF605F", "#FFD52E", "#49DD8E", "#AE99FF", "#4CC3D9", "#CF6CC7", "#DB6597", "#4BABBD", "#EA9BE1", "#FFB6C1", "#FFE4E1", "#F2F2F3", "#EBD3C7", "#5BA68C", "#EBEF89", "#92EACC", "#62D7D2", "#ABB1E8", "#D0E4B7"];
         var renderData = [];
         for (var i = 0; i < colors.length; i++) {
-            renderData.push(<Circle bgColor={colors[i]} /><Label bgColor={colors[i]}/> );
+        renderData.push(<Card color={colors[i]} />);
         }
         return (
             <div>
